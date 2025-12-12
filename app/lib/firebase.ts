19f565aeab88ce863467c8585a926bdd2cdb13d5
Dan Chavez <thenameisdna@gmail.com>
2025-12-12 21:09:13 +0800

    Refactor Firebase configuration to use environment variables directly
    - Updated Firebase configuration to remove default values and ensure all keys are sourced from environment variables.
    - This change enhances security by preventing sensitive information from being hardcoded in the source code.

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getDatabase } from 'firebase/database';
 // Firebase configuration
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBw112HUm552l4rmD7Nex3IsX7SHm0iVHg",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "cafeos-8cbfb.firebaseapp.com",
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "https://cafeos-8cbfb-default-rtdb.firebaseio.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "cafeos-8cbfb",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "cafeos-8cbfb.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "423083414516",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:423083414516:web:51ee75105ac1d4033b1fa1",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-NFTN17HDLZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
